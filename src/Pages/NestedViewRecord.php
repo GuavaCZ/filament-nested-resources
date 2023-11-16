576c71d2916e8012723971af6059c6745d2ec163
Dan Poblete <ace.poblete@gmail.com>
2023-11-16 11:17:34 -0600

    updating edit action to use getNormalizedRouteParameters

--- src/Pages/NestedViewRecord.php
+++ src/Pages/NestedViewRecord.php
@@ -2,9 +2,27 @@
 
 namespace Guava\Filament\NestedResources\Pages;
 
+use Filament\Forms\Form;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class NestedViewRecord extends ViewRecord
 {
     use NestedPage;
+
+    protected function configureEditAction(EditAction $action): void
+    {
+        $resource = static::getResource();
+
+        $action
+            ->authorize($resource::canEdit($this->getRecord()))
+            ->form(fn (Form $form): Form => static::getResource()::form($form));
+
+        if ($resource::hasPage('edit')) {
+            $action->url(fn (): string => static::getResource()::getUrl('edit', [
+                ...static::getResource()::getAncestor()->getNormalizedRouteParameters($this->getRecord()),
+                'record' => $this->getRecord()
+            ]));
+        }
+    }
 }
