8fc9eb420a896eebc179bc878b9691ea2163a51a
Lukas Frey <mail@lukasfrey.cz>
2023-11-18 19:55:32 +0100

    fix: conditional action URLs when their pages don't exist
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- src/Pages/NestedListRecords.php
+++ src/Pages/NestedListRecords.php
@@ -3,6 +3,7 @@
 namespace Guava\Filament\NestedResources\Pages;
 
 use Filament\Actions\CreateAction;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -15,16 +16,56 @@ class NestedListRecords extends ListRecords
     protected function makeTable(): Table
     {
         return parent::makeTable()
-            ->recordUrl(fn (Model $record) => static::getResource()::getUrl('edit', [
-                ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
-                'record' => $record,
-            ]))
+            ->when(
+                static::getResource()::hasPage('edit'),
+                fn (Table $table) => $table->recordUrl(fn (Model $record) => static::getResource()::getUrl('edit', [
+                    ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
+                    'record' => $record,
+                ])),
+                fn (Table $table) => $table->recordUrl(null)
+            )
+        ;
+    }
+
+    protected function configureViewAction(Tables\Actions\ViewAction | EditAction $action): void
+    {
+        parent::configureViewAction($action);
+        $action
+            ->when(
+                static::getResource()::hasPage('view'),
+                fn (Tables\Actions\ViewAction $action) => $action
+                    ->url(fn ($record) => static::getResource()::getUrl('view', [
+                        ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
+                        'record' => $record,
+                    ]))
+            )
         ;
     }
 
     protected function configureCreateAction(Tables\Actions\CreateAction | CreateAction $action): void
     {
         parent::configureCreateAction($action);
-        $action->url(static::getResource()::getUrl('create', $this->getRouteParameterIds()));
+        $action
+            ->when(
+                static::getResource()::hasPage('create'),
+                fn (CreateAction $action) => $action
+                    ->url(static::getResource()::getUrl('create', $this->getRouteParameterIds())),
+                fn (CreateAction $action) => $action->hidden()
+            )
+        ;
+    }
+
+    protected function configureEditAction(Tables\Actions\EditAction | EditAction $action): void
+    {
+        parent::configureEditAction($action);
+        $action
+            ->when(
+                static::getResource()::hasPage('edit'),
+                fn (Tables\Actions\EditAction $action) => $action
+                    ->url(fn ($record) => static::getResource()::getUrl('edit', [
+                        ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
+                        'record' => $record,
+                    ]))
+            );
     }
 }
--- src/RelationManagers/NestedRelationManager.php
+++ src/RelationManagers/NestedRelationManager.php
@@ -17,29 +17,41 @@ class NestedRelationManager extends RelationManager
     protected function configureViewAction(Tables\Actions\ViewAction $action): void
     {
         parent::configureViewAction(
-            $action->url(fn (Model $record) => static::getResource()::getUrl('view', [
-                ...static::getResource()::getAncestor()->getNormalizedRouteParameters($this->getOwnerRecord()),
-                'record' => $record,
-            ]))
+            $action
+                ->when(
+                    static::getResource()::hasPage('view'),
+                    fn (Tables\Actions\ViewAction $action) => $action->url(fn (Model $record) => static::getResource()::getUrl('view', [
+                        ...static::getResource()::getAncestor()->getNormalizedRouteParameters($this->getOwnerRecord()),
+                        'record' => $record,
+                    ]))
+                )
         );
     }
 
     protected function configureCreateAction(Tables\Actions\CreateAction $action): void
     {
         parent::configureCreateAction(
-            $action->url(static::getResource()::getUrl('create', [
-                ...static::getResource()::getAncestor()->getNormalizedRouteParameters($this->getOwnerRecord()),
-            ]))
+            $action
+                ->when(
+                    static::getResource()::hasPage('create'),
+                    fn (Tables\Actions\CreateAction $action) => $action->url(static::getResource()::getUrl('create', [
+                        ...static::getResource()::getAncestor()->getNormalizedRouteParameters($this->getOwnerRecord()),
+                    ]))
+                )
         );
     }
 
     protected function configureEditAction(Tables\Actions\EditAction $action): void
     {
         parent::configureEditAction(
-            $action->url(fn (Model $record) => static::getResource()::getUrl('edit', [
-                ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
-                'record' => $record,
-            ]))
+            $action
+                ->when(
+                    static::getResource()::hasPage('edit'),
+                    fn (Tables\Actions\EditAction $action) => $action->url(fn (Model $record) => static::getResource()::getUrl('edit', [
+                        ...static::getResource()::getAncestor()->getNormalizedRouteParameters($record),
+                        'record' => $record,
+                    ]))
+                )
         );
     }
 }
